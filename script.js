d3256c7d3041f7a404a72d33208d64158b619275
Gvbr1el <castorgabriel25@gmail.com>
2023-05-18 18:38:06 -0300

    cronometro regressivo

---
+++ script.js
@@ -0,0 +1,35 @@
+var min = 4;
+var seg = 00;
+
+function promocao(){
+    var titulo = document.getElementsByTagName("h1")[0];
+    var contador = document.getElementsByTagName("p")[0];
+    contador.innerText = `${min}:${seg}`
+    
+    if(seg === 0){
+        min--;
+        seg = 60
+    }
+    else{
+        seg--;
+    }
+
+    if(min === 0 && seg === 0){
+        titulo.innerText = "A promoção chegou ao fim!";
+        min = 0;
+        seg = 0;
+        contador.innerText = `${min}:${seg}`
+        stopInterval();
+    }
+    
+}
+
+var inter;
+
+function interval(){
+    inter = setInterval(promocao, 1000);
+}
+
+function stopInterval(){
+    clearInterval(inter);
+}
